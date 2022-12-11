d9505a0c7d51eba739e8adfc88b88a845a079e89
Jeonghoon CHOI <wjdgnsdldyd74@gmail.com>
2022-12-11 17:04:54 +0900

    change

--- app/src/main/java/com/example/baedalpot/ui/home/child/SerchFragment.java
+++ app/src/main/java/com/example/baedalpot/ui/home/child/SerchFragment.java
@@ -150,6 +150,7 @@ public class SerchFragment extends Fragment implements ValueEventListener{
                     }
                 });
                 startActivity(new Intent(requireContext(), NaviActivity.class));
+                
             }
         });
 
